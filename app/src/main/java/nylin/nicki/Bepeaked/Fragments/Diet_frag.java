776d21003264ca6f61eda95489557049d5230d37
Nicki N <nickinylin@hotmail.com>
2016-11-07 11:56:17 +0100

    Slettet log ud i menuen

--- app/src/main/java/nylin/nicki/Bepeaked/Fragments/Diet_frag.java
+++ app/src/main/java/nylin/nicki/Bepeaked/Fragments/Diet_frag.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
 
 import nylin.nicki.Bepeaked.R;
 
@@ -24,7 +26,63 @@ public class Diet_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ExpandableListAdapter mAdapter = new MyExpandableListAdapter();
+
         return inflater.inflate(R.layout.fragment_diet, container, false);
     }
 
-}
+    private String[] groups = {"Morgenmad", "Frokost", "Aftensmad"};
+
+    public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+
+
+        @Override
+        public int getGroupCount() {
+            return 0;
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return 0;
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return null;
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return null;
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return 0;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return false;
+        }
+    }
