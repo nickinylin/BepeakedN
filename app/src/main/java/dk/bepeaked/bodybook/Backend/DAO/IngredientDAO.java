e865b6a393f1de3d92e38ecb27a5527ee3d9155d
Nicki N <nickinylin@hotmail.com>
2016-11-14 12:54:48 +0100

    Tilføjet flere attr. og metoder til DTO'er

---
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/IngredientDAO.java
@@ -0,0 +1,8 @@
+package dk.bepeaked.bodybook.Backend.DAO;
+
+/**
+ * Created by Nicki on 14/11/16.
+ */
+
+public class IngredientDAO {
+}
--- app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/DishDTO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/DishDTO.java
@@ -1,9 +1,19 @@
 package dk.bepeaked.bodybook.Backend.DTO;
 
+import java.util.ArrayList;
+
 /**
  * Created by sebho on 14-11-2016.
  */
 
 public class DishDTO {
+    private int ID;
+    private String name;
+    private ArrayList<IngredientDTO> ingredients;
 
+    public DishDTO (int ID, String name, ArrayList<IngredientDTO> ingredients) {
+        this.ID = ID;
+        this.name = name;
+        this.ingredients = ingredients;
+    }
 }
