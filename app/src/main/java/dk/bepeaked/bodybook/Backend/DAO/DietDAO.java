61e7d5238d2c8266a15e38f8b8628e0cf8a4afef
hoppe2808 <seb.hop@hotmail.com>
2016-11-14 12:22:32 +0100

    DAO og DTO klasser

--- app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
@@ -1,8 +1,18 @@
 package dk.bepeaked.bodybook.Backend.DAO;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import dk.bepeaked.bodybook.R;
+
 /**
  * Created by sebho on 14-11-2016.
  */
 
 public class DietDAO {
+    private void createJSON() throws IOException, JSONException {
+        JSONObject json = new JSONObject();
+    }
 }
