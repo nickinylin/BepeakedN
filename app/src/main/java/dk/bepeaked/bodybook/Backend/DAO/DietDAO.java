2311e5b94728798ebca59db64be1e56958ecfee2
Nicki N <nickinylin@hotmail.com>
2016-11-17 16:42:01 +0100

    Flyttet ingredientDTO ind i dishDTO

--- app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
@@ -1,5 +1,6 @@
 package dk.bepeaked.bodybook.Backend.DAO;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -15,5 +16,10 @@ public class DietDAO {
     private void createJSON() throws IOException, JSONException {
         JSONObject json = new JSONObject();
 
+        JSONArray ja = json.getJSONArray(String.valueOf(R.raw.morgenmad));
+
+        for (int i = 0; i < ja.length(); i++){
+            ja.get(i);
+        }
     }
 }
--- app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/DishDTO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/DishDTO.java
@@ -9,11 +9,81 @@ import java.util.ArrayList;
 public class DishDTO {
     private int ID;
     private String name;
-    private ArrayList<IngredientDTO> ingredients;
+    private ArrayList<Ingredient> ingredients;
 
-    public DishDTO (int ID, String name, ArrayList<IngredientDTO> ingredients) {
+    public DishDTO(int ID, String name, ArrayList<Ingredient> ingredients) {
         this.ID = ID;
         this.name = name;
         this.ingredients = ingredients;
     }
+
+
+    // Private inner class
+    private class Ingredient {
+
+        private String name;
+        private int weight;
+        private int protein;
+        private int fat;
+        private int carbohydrate;
+        private int calories;
+
+        public Ingredient(String name, int weight, int protein, int fat, int carbohydrate, int calories) {
+            this.name = name;
+            this.weight = weight;
+            this.protein = protein;
+            this.fat = fat;
+            this.carbohydrate = carbohydrate;
+            this.calories = calories;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public int getProtein() {
+            return protein;
+        }
+
+        public void setProtein(int protein) {
+            this.protein = protein;
+        }
+
+        public int getFat() {
+            return fat;
+        }
+
+        public void setFat(int fat) {
+            this.fat = fat;
+        }
+
+        public int getCarbohydrate() {
+            return carbohydrate;
+        }
+
+        public void setCarbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+        }
+
+        public int getCalories() {
+            return calories;
+        }
+
+        public void setCalories(int calories) {
+            this.calories = calories;
+        }
+    }
+
 }
