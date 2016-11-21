ba95e75daca5d203fef050009d041c0436166310
Nicki N <nickinylin@hotmail.com>
2016-11-21 08:44:04 +0100

    Update2

--- app/src/main/java/dk/bepeaked/bodybook/Fragments/Diet/Diet_frag.java
+++ app/src/main/java/dk/bepeaked/bodybook/Fragments/Diet/Diet_frag.java
@@ -1,17 +1,15 @@
 package dk.bepeaked.bodybook.Fragments.Diet;
 
 
-import android.app.ExpandableListActivity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
@@ -19,7 +17,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import dk.bepeaked.bodybook.Activities.MyAdapter;
 import dk.bepeaked.bodybook.R;
 
 /**
@@ -90,5 +87,92 @@ public class Diet_frag extends Fragment {
     public void onPrepareOptionsMenu(Menu menu) {
         menu.findItem(R.id.action_settings).setVisible(false);
         super.onPrepareOptionsMenu(menu);
+    }
+
+    /**
+     * Created by Nicki on 18/11/16.
+     */
+
+    public static class MyAdapter extends BaseExpandableListAdapter {
+
+        private List<String> header_titles;
+        private HashMap<String, List<String>> child_titles;
+        private Context ctx;
+
+        public MyAdapter(Context ctx, List<String> header_titles, HashMap<String,List<String>> child_titles) {
+            this.ctx = ctx;
+            this.child_titles = child_titles;
+            this.header_titles = header_titles;
+        }
+
+        @Override
+        public int getGroupCount() {
+            return header_titles.size();
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return child_titles.get(header_titles.get(groupPosition)).size();
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return header_titles.get(groupPosition);
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return child_titles.get(header_titles.get(groupPosition)).get(childPosition);
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            String title =  (String) this.getGroup(groupPosition);
+            if (convertView == null){
+                LayoutInflater layoutInflater = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.parent_layout, null);
+            }
+            TextView textView = (TextView) convertView.findViewById(R.id.heading_item);
+            textView.setTypeface(null, Typeface.BOLD);
+            textView.setText(title);
+
+
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            String title = (String) this.getChild(groupPosition, childPosition);
+
+            if(convertView == null) {
+                LayoutInflater layoutInflater = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.child_layout, null);
+            }
+
+            TextView textView = (TextView) convertView.findViewById(R.id.child_item);
+            textView.setText(title);
+
+            return convertView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
     }
 }
