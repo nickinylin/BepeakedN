3c332c687b54ef101f8faa3d6810cd2dceaff1d4
Big Fat Llama <lukas@10.16.99.208>
2016-11-23 11:41:27 +0100

    tilføjet en masse tror jeg

---
+++ app/src/main/java/tests/JUnit_datalayer.java
@@ -0,0 +1,9 @@
+package tests;
+
+//import org.junit.Test;
+//import java.util.regex.Pattern;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+
+public class JUnit_datalayer {
+}
