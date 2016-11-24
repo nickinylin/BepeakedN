77e14699b50d5c128949c98481147353a472015b
hoppe2808 <seb.hop@hotmail.com>
2016-11-24 15:20:40 +0100

    Updates

--- app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DAO/DietDAO.java
@@ -23,26 +23,16 @@ import dk.bepeaked.bodybook.R;
 
 public class DietDAO {
     public ArrayList<DishDTO> getDishes(Activity act) throws IOException, JSONException {
-
-
-
         ArrayList<DishDTO> result = new ArrayList<DishDTO>();
-
         InputStream is = act.getResources().openRawResource(R.raw.dish);
-        //InputStream is = new URL("http://javabog.dk/eksempel.json").openStream();
-
         byte b[] = new byte[is.available()]; // kun små filer
         is.read(b);
         String str = new String(b, "UTF-8");
-
         JSONObject json = new JSONObject();
         JSONObject jA = new JSONObject(str);
         JSONArray category;
         JSONObject dish;
-        Log.d("Sebbyg", "getDishes: " + jA.length());
 
-
-//        Iterator<String> iterator =
         for (int i = 0; i < jA.length(); i++){
             category = jA.getJSONArray("Morgenmad");
             switch (i){
@@ -60,7 +50,6 @@ public class DietDAO {
                 String name = dish.getString("name");
                 String imagePath = dish.getString("image");
                 JSONArray ingredientList = dish.getJSONArray("Ingredients");
-
                 ArrayList<String[]> ingredients = new ArrayList<String[]>();
                 for (int k = 0; k < ingredientList.length(); k++){
                     JSONObject ingredient = ingredientList.getJSONObject(k);
@@ -71,5 +60,4 @@ public class DietDAO {
         }
         return result;
     }
-
 }
