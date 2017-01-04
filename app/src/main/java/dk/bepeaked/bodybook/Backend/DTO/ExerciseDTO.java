4344d776a58acdca43b6430898e153f705c71ce3
hoppe2808 <seb.hop@hotmail.com>
2017-01-04 10:36:11 +0100

    Arbejde med backend

--- app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/ExerciseDTO.java
+++ app/src/main/java/dk/bepeaked/bodybook/Backend/DTO/ExerciseDTO.java
@@ -1,5 +1,9 @@
 package dk.bepeaked.bodybook.Backend.DTO;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -8,39 +12,62 @@ import java.util.Date;
 
 public class ExerciseDTO {
 
-    private String name;
+    private String name, desc;
+    private ArrayList<Set> set;
+    private DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+
+    public ExerciseDTO(String name, String desc, ArrayList<String[]> data){
+        this.name = name;
+        this.desc = desc;
+
+        for(int i = 0; i < data.size(); i++){
+            String[] singleSet = data.get(i);
+            Date date = null;
+            try {
+                date = df.parse(singleSet[2]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            set.add(new Set(Integer.parseInt(singleSet[0]), Integer.parseInt(singleSet[1]), date));
+        }
+    }
+
     //TODO LAV SOM I ExerciseDAO
-    private int weight, reps;
-    private Date date;
 
-    public ExerciseDTO (int weight, int reps, Date date) {
-        this.weight = weight;
-        this.reps = reps;
-        this.date = date;
-    }
 
-    public int getWeight() {
-        return weight;
-    }
+    public class Set{
+        private int weight, reps;
+        private Date date;
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
+        public Set (int weight, int reps, Date date) {
+            this.weight = weight;
+            this.reps = reps;
+            this.date = date;
+        }
 
-    public int getReps() {
-        return reps;
-    }
+        public int getWeight() {
+            return weight;
+        }
 
-    public void setReps(int reps) {
-        this.reps = reps;
-    }
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
 
-    public Date getDate() {
-        return date;
-    }
+        public int getReps() {
+            return reps;
+        }
 
-    public void setDate(Date date) {
-        this.date = date;
+        public void setReps(int reps) {
+            this.reps = reps;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
     }
 
 }
